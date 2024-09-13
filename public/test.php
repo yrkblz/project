027a439e8ee30ef952918a73091b126904777df3
test <test@>
2024-09-13 12:28:59 +0200

    front

--- public/test.php
+++ public/test.php
@@ -1,14 +1,16 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$pass  = "";
+
+$conn =new mysqli_connect($servername, $username, $pass);
+
+if ($conn->conect_error){
 
 
+    die("failed": ".mysqli_connect_error()");
+}
+echo "successfully"
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    
-</body>
-</html>
+
+?>
